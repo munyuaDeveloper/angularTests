5aee340f5e125743ef6299913786a3cb3d8f4d5d
munyua <munyuapeter07@gmail.com>
2021-03-05 15:59:21 +0300

    title should be contained in h3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {By} from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -20,16 +21,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularTests'`, () => {
+  it('should show Application Title in h3 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularTests');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AngularTests app is running!');
+    const de = fixture.debugElement.query(By.css('h3'));
+    expect(de.nativeElement.textContent).toEqual('Application Title');
   });
 });
